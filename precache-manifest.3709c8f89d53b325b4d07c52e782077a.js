fca6dc4e008fabb7dc518cca2a405a06337e5a96
om <om@bigappcompany.in>
2019-08-23 19:59:39 +0530

    Updates

---
+++ precache-manifest.3709c8f89d53b325b4d07c52e782077a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc058ced825695f13f20efb7bfb23423",
+    "url": "/plej/index.html"
+  },
+  {
+    "revision": "c4b592bece0532bc0c17",
+    "url": "/plej/static/css/2.3c1ddb0a.chunk.css"
+  },
+  {
+    "revision": "2e3bbac684e4afcffc61",
+    "url": "/plej/static/css/main.833b10b1.chunk.css"
+  },
+  {
+    "revision": "c4b592bece0532bc0c17",
+    "url": "/plej/static/js/2.5ff040a3.chunk.js"
+  },
+  {
+    "revision": "2e3bbac684e4afcffc61",
+    "url": "/plej/static/js/main.dc79ad62.chunk.js"
+  },
+  {
+    "revision": "a31a240ae7950f79fc67",
+    "url": "/plej/static/js/runtime~main.8395a4f9.js"
+  }
+]);
