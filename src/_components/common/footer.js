f04dc19481edc598e8bd0a9fb43e4b106f5d3a46
strider99 <panigrahiomprakash@gmail.com>
2019-10-04 15:52:07 +0530

    feat - newsletter form

--- src/_components/common/footer.js
+++ src/_components/common/footer.js
@@ -1,6 +1,49 @@
 import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
+import axios from "axios";
+import { formTypes } from "../../_constants/formTypes";
+import { api } from "../../env";
+import {toast} from 'react-toastify';
 class Footer extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            fullName: "",
+            email: "",
+            isSubmitting: false
+        }
+        window.scrollTo({ top: 0, behavior: "smooth" });
+    }
+
+    handleInputChange = event => {
+        let {value} = event.target;
+        this.setState({
+            [event.target.name]: value
+        }) ;
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        this.setState({isSubmitting: true});
+        let url = `${api.apiCompany}general/send-email`;
+        let payload = {
+            fullName: this.state.fullName,
+            email: this.state.email,
+            formType : formTypes.NEWS_LETTER
+
+        }
+
+        axios.post(url, payload)
+          .then(res => {
+              if(res.status == 200){
+                  toast.success(res.data.message);
+              }
+          })
+          .catch(err => {
+              console.log('the error is ', err)
+          })
+
+    }
     render() {
         return (
             <div>
@@ -143,22 +186,22 @@ class Footer extends Component {
                             <form class="_3CID_bm">
                                 <div class="_2nxeEK_ _14NUrhZ">
                                     {/* <label for="name"></label> */}
-                                    <input type="text" name="name" placeholder="Your Name" vlaue="" />
+                                    <input onChange={this.handleInputChange} type="text" name="fullName" placeholder="Your Name" vlaue="" />
                                     {/* <input type="text" name="name" value=""
                                         placeholder="Your name" />
                                     <small class="_22qkbgJ"></small> */}
                                 </div>
                                 <div class="_2nxeEK_ _14NUrhZ">
-                                    <input type="text" name="name" placeholder="Your Email" vlaue="" />
+                                    <input onChange={this.handleInputChange} type="text" name="email" placeholder="Your Email" vlaue="" />
                                     {/* <label for="email"></label>
                                     <input type="email" name="email" value=""
                                         placeholder="Your email" />
                                     <small class="_22qkbgJ"></small> */}
                                 </div>
                                 <div className="pt-16">
-                                    <NavLink class="Yd7bNNG _3Pq3GhV button _1x8JHAI" to="#">
+                                    <button onClick={this.handleSubmit} class="Yd7bNNG _3Pq3GhV button _1x8JHAI">
                                         <span>Submit</span>
-                                    </NavLink>
+                                    </button>
                                 </div>
                             </form>
                         </div>
