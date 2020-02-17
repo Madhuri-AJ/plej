e565ec090f0909d2b3cdd2dcaf511254c6e848ba
Surya <surya.t@bigappcompany.in>
2020-02-17 13:45:34 +0530

    added react-ga for google analytics

--- src/index.js
+++ src/index.js
@@ -1,7 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactGA from 'react-ga';
+// import TagManager from 'react-gtm-module';
 import App from './App';
+import {history} from './_components/history';
 import * as serviceWorker from './serviceWorker';
+
+
+const trackingId = "UA-155076072-1";
+ReactGA.initialize(trackingId, {standardImplementation:true});
+
+// const tagManagerArgs = {
+//     gtmId: 'GTM-XXXXXX'
+// }
+
+// TagManager.initialize(tagManagerArgs)
+
+history.listen(location => {
+    ReactGA.set({page : location.pathname});
+    ReactGA.pageview(location.pathname);
+})
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
